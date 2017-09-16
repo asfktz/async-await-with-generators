2b7f94772f8cbe27a9327a568b30691b621d059d
asaf <asf.ktz@gmail.com>
2017-09-16 15:09:40 +0300

    init

---
+++ index.js
@@ -0,0 +1,19 @@
+function next(it, res, err) {
+  const { value, done } = err ? it.throw(err) : it.next(res);
+
+  if (done) {
+    return Promise.resolve(value);
+  }
+
+  return Promise.resolve(value)
+    .then(res => {
+      return next(it, res);
+    })
+    .catch(err => {
+      return next(it, null, err);
+    });
+}
+
+module.exports = function run(gen) {
+  return next(gen());
+}
